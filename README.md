fb58beeb5af8e6811b5564f5c0d002205b916aba
Nanna Setämaa <nanna.karoliina@outlook.com>
2023-03-03 14:49:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# DS project
+
+## Setting up the development environment
+
+Run the script to setup the development environment.
+
+```shell
+chmod +x setup.sh
+./setup.sh
+```
+
+## Dependency control
+
+Note: ./setup.sh syncs these dependencies automatically when run,
+
+### Sync dependencies
+```shell
+pip-sync
+```
+
+### Add or update dependencies
+```shell
+pip-compile
+```
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+set +x
+
+echo "Setting up the dev env for project"
+
+if [ ! -d "venv" ]; then
+  echo "Creating the env... "
+  python3 -m venv venv
+fi
+
+if [ -d "venv" ]; then
+  echo "Activating the venv..."
+  source venv/bin/activate
+
+  if [ "$(pip list | grep -F pip-tools)" ]; then
+    echo "Pip-tools installed!"
+  else
+    echo "Install pip-tools for requirements management..."
+    pip install pip-tools
+  fi
+
+  if [ -f "requirements.txt" ]; then
+    echo "Install pip dependencies..."
+    pip-sync
+  fi
+fi
